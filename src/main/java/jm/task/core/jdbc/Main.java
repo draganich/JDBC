911db4631128e5d44eb5bc787cbf1478e702a3b6
Ziganshin Alik <ziganshin.af19@physics.msu.ru>
2022-12-22 05:58:42 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,20 +1,22 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
         Util.getConnection();
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
 
-        userDaoJDBC.saveUser("Alex", "Smith", (byte) 21);
-        userDaoJDBC.saveUser("John", "Brown", (byte) 54);
-        userDaoJDBC.saveUser("Kara", "Johnson", (byte) 13);
-        userDaoJDBC.saveUser("Bill", "Williams", (byte) 29);
+        userService.saveUser("Alex", "Smith", (byte) 21);
+        userService.saveUser("John", "Brown", (byte) 54);
+        userService.saveUser("Kara", "Johnson", (byte) 13);
+        userService.saveUser("Bill", "Williams", (byte) 29);
 
-        userDaoJDBC.getAllUsers();
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
